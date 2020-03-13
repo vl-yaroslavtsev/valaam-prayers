3005750786ff2905300ebfbae5d36e4eb25ea8be
vl-yaroslavtsev <55184140+vl-yaroslavtsev@users.noreply.github.com>
2020-03-13 16:31:23 +0300

    Обновление скриптов сборки для устройства

---
+++ scripts/build.prod.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp ../../prayers.v2.f7/framework7.phonegap.js ./src/ || exit 1
+webpack --env.production || exit 1
+rm -rf ../../prayers.v2.f7/* || exit 1
+cp -a ./dist/. ../../prayers.v2.f7/ || exit 1
+rm -rf ../../prayers.v2.f7/cordova.js || exit 1
