1e01929cabc7be6003e5c177087635b434a0b1d8
vl-yaroslavtsev <55184140+vl-yaroslavtsev@users.noreply.github.com>
2020-03-26 16:08:50 +0300

    Полноэкранный режим в настройках

--- src/js/settings-manager.js
+++ src/js/settings-manager.js
@@ -2,12 +2,30 @@
  * Управляет настройками, которые были сохранены на странице
  * Настройки
  */
+import {Dom7 as $$} from 'framework7';
 
 let app;
 
 function init(appInstance) {
 	app = appInstance;
+	setDefault();
 	apply();
+}
+
+function setDefault() {
+	let settings = app.methods.storageGet('settings') || {};
+	
+	if (!('hideStatusbar' in settings)) {
+		settings.hideStatusbar = true;
+	}
+	
+	save(settings);
+}
+
+function get(key) {
+	let settings = app.methods.storageGet('settings') || {};
+	
+	return settings[key];
 }
 
 function save(item = {}) {
@@ -30,15 +48,25 @@ function apply(state) {
 }
 
 function applyTheme({themeDark}) {
+	let currentView = app.views.current,
+		$page, isWhitePage;
+	
+	if (currentView) {
+		$page = $$(currentView.router.currentPageEl);
+		isWhitePage = $page.hasClass('page-white');
+	}
+	
 	if (themeDark) {
-		document.querySelector('html').classList.add('theme-dark');
-		document.querySelector('meta[name="theme-color"]').content = '#202020';
-		document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]').content = 'black';
+		$$('html').addClass('theme-dark');
+		//document.querySelector('meta[name="theme-color"]').content = '#202020';
+		//document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]').content = 'black';
+		app.statusbar.setTextColor('white');
 
 	} else {
-		document.querySelector('html').classList.remove('theme-dark');
-		document.querySelector('meta[name="theme-color"]').content = '#3878a8';
-		document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]').content = 'default';
+		$$('html').removeClass('theme-dark');
+		//document.querySelector('meta[name="theme-color"]').content = '#3878a8';
+		//document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]').content = 'default';
+		app.statusbar.setTextColor(isWhitePage ? 'black' : 'white');
 	}
 }
 
@@ -54,15 +82,14 @@ function applyStyles({
 	bgImage,
 	bgColor
 }) {
-	let isThemeDark = document.querySelector('html')
-			.classList.contains('theme-dark');
-	let style = document.querySelector('#app-settings-style');
-	if (style) {
+	let isThemeDark = $$('html').hasClass('theme-dark');
+	let style = $$('#app-settings-style');
+	if (style.length) {
 		style.remove();
 	}
-	style = document.createElement('style');
-	style.id = 'app-settings-style';
-	style.innerHTML = `
+	
+	let styleHTML = `
+	<style id="app-settings-style">
 		.md .view:not(#view-valaam) .text-content {
 			${slFontFamily ? 'font-family: "' + slFontFamily + '" !important;' : ''}
 			${slFontSize ? 'font-size: ' + slFontSize + 'px !important;' : ''}
@@ -84,8 +111,9 @@ function applyStyles({
 			${!isThemeDark && bgImage ? 'background-image: ' + bgImage + ' !important;' : ''}
 			${!isThemeDark && bgColor ? 'background-color: ' + bgColor + ' !important;' : ''}
 		}
+	</style>
 	`;
-	document.head.appendChild(style);
+	$$('head').append(styleHTML);
 }
 
-export default {init, save, apply};
+export default {init, get, save, apply};
