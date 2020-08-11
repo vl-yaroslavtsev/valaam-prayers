3cd5c9677ec372118a817d6cff164da96da422fe
Владимир Ярославцев <webmaster@valaam.ru>
2020-08-11 12:52:18 +0300

    Загрузка данных: закрытие диалога по окончании

--- src/js/app.js
+++ src/js/app.js
@@ -113,7 +113,7 @@ const app = new Framework7({
 	name: 'Валаамский молитвослов',
 	theme: navigator.userAgent.match(/Debug/) !== null ? 'auto' : 'md',
 	disabled: false,
-	version: "1.11.8",
+	version: "1.11.9",
 	// theme: 'ios',
 
 	statusbar: {
