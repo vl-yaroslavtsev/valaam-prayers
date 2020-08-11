7724696d5ecb7e195ba8ce00d816aa5aa7f144b5
Владимир Ярославцев <webmaster@valaam.ru>
2020-08-11 12:33:34 +0300

    Настройки текста: убрано затеменение для popup и popover

--- src/js/app.js
+++ src/js/app.js
@@ -113,7 +113,7 @@ const app = new Framework7({
 	name: 'Валаамский молитвослов',
 	theme: navigator.userAgent.match(/Debug/) !== null ? 'auto' : 'md',
 	disabled: false,
-	version: "1.11.5",
+	version: "1.11.6",
 	// theme: 'ios',
 
 	statusbar: {
