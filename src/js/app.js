ebd75e4a7431c682aca8225ceeab15c319326a7e
Владимир Ярославцев <webmaster@valaam.ru>
2020-08-14 16:38:30 +0300

    Доработка виртуального списка

--- src/js/app.js
+++ src/js/app.js
@@ -113,7 +113,7 @@ const app = new Framework7({
 	name: 'Валаамский молитвослов',
 	theme: navigator.userAgent.match(/Debug/) !== null ? 'auto' : 'md',
 	disabled: false,
-	version: "1.11.11",
+	version: "1.11.12",
 	// theme: 'ios',
 
 	statusbar: {
