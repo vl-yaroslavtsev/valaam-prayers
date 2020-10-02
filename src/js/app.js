ebb2f6034994b875e33fe6855e7934d24ddf08d1
unknown <vl.yaroslavtsev@gmail.com>
2020-10-02 22:52:50 +0300

    text-settings: исправление ошибки расчета высоты на больших экранах

--- src/js/app.js
+++ src/js/app.js
@@ -116,7 +116,7 @@ const app = new Framework7({
 	name: 'Валаамский молитвослов',
 	theme: navigator.userAgent.match(/Debug/) !== null ? 'auto' : 'md',
 	disabled: false,
-	version: "1.14.6",
+	version: "1.14.7",
 	// theme: 'ios',
 
 	statusbar: {
