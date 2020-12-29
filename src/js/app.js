e0643d8472ced5d6e832358e16719074cb4f32f8
unknown <vl.yaroslavtsev@gmail.com>
2020-12-29 21:38:41 +0300

    Исправлены цвета дней

--- src/js/app.js
+++ src/js/app.js
@@ -120,7 +120,7 @@ const app = new Framework7({
 	name: 'Валаамский молитвослов',
 	theme: navigator.userAgent.match(/Debug/) !== null ? 'auto' : 'md',
 	disabled: false,
-	version: "1.16.1",
+	version: "1.16.2",
 	// theme: 'ios',
 
 	statusbar: {
