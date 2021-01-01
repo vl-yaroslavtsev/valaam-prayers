bb643c724620e00771121a66f840df8c8ac17c65
unknown <vl.yaroslavtsev@gmail.com>
2021-01-01 12:34:41 +0300

    Тропари и кондаки в календаре из молитвослова

--- src/js/app.js
+++ src/js/app.js
@@ -120,7 +120,7 @@ const app = new Framework7({
 	name: 'Валаамский молитвослов',
 	theme: navigator.userAgent.match(/Debug/) !== null ? 'auto' : 'md',
 	disabled: false,
-	version: "1.16.2",
+	version: "1.17.1",
 	// theme: 'ios',
 
 	statusbar: {
