4c4e250651b40872cc137b0c1886b32ac525c3d9
unknown <vl.yaroslavtsev@gmail.com>
2021-02-08 14:06:00 +0300

    Порядок чтений название

--- src/js/app.js
+++ src/js/app.js
@@ -120,7 +120,7 @@ const app = new Framework7({
 	name: 'Валаамский молитвослов',
 	theme: navigator.userAgent.match(/Debug/) !== null ? 'auto' : 'md',
 	disabled: false,
-	version: "1.17.1",
+	version: "1.17.2",
 	// theme: 'ios',
 
 	statusbar: {
