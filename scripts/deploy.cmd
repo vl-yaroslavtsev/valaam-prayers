9417a322f61167a7a4931752c792220511af9e41
Владимир Ярославцев <vl.yaroslavtsev@gmail.com>
2025-08-25 14:38:26 +0300

    Обновлен скрипт деплоя на новый сервер

--- scripts/deploy.cmd
+++ scripts/deploy.cmd
@@ -1,14 +1,14 @@
 @echo off
 chcp 65001
 
-set KEY="C:\Users\Владимир\.ssh\id_rsa_valaam"
-set USER="valaam_vladimir.ya@valaam.ru"
-set REMOTE_FOLDER="/pub/home/valaam/htdocs.molitvoslov"
+set KEY="C:\Users\Владимир\.ssh\id_rsa_valaam_ru"
+set USER="valaam.ru@valaam.ru"
+set REMOTE_FOLDER="/var/www/molitvoslov.valaam.ru/data"
 
 
 ssh -i %KEY% %USER% "rm -rf %REMOTE_FOLDER%/app/*"
 scp -r -i %KEY% ./www/* %USER%:%REMOTE_FOLDER%/app/
-ssh -i %KEY% %USER% "find %REMOTE_FOLDER%/app/ -type d -exec chmod 4750 {} \;"
+ssh -i %KEY% %USER% "find %REMOTE_FOLDER%/app/ -type d -exec chmod 4755 {} \;"
 
 rem ssh -i %KEY% %USER% "rm -rf %REMOTE_FOLDER%/index.html %REMOTE_FOLDER%/service-worker.js %REMOTE_FOLDER%/icons/* %REMOTE_FOLDER%/assets/*"
 rem scp -r -i %KEY% ./www/* %USER%:%REMOTE_FOLDER%/
