08d88a1e55844e9f0979167e5beb5be12a5993a2
Владимир Ярославцев <vl.yaroslavtsev@gmail.com>
2026-08-08 17:53:03 +0300

    Убираем лишнее в deploy скрипте

--- scripts/deploy.cmd
+++ scripts/deploy.cmd
@@ -1,7 +1,6 @@
 @echo off
 chcp 65001
 
-rem set KEY="C:\Users\Владимир\.ssh\id_rsa_valaam_ru"
 set USER="valaam.ru@valaam.ru"
 set REMOTE_FOLDER="/var/www/app.valaam.ru/data"
 
@@ -9,7 +8,3 @@ set REMOTE_FOLDER="/var/www/app.valaam.ru/data"
 ssh %USER% "rm -rf %REMOTE_FOLDER%/app/*"
 scp -r ./www/* %USER%:%REMOTE_FOLDER%/app/
 ssh %USER% "find %REMOTE_FOLDER%/app/ -type d -exec chmod 4755 {} \;"
-
-rem ssh -i %KEY% %USER% "rm -rf %REMOTE_FOLDER%/index.html %REMOTE_FOLDER%/service-worker.js %REMOTE_FOLDER%/icons/* %REMOTE_FOLDER%/assets/*"
-rem scp -r -i %KEY% ./www/* %USER%:%REMOTE_FOLDER%/
-rem ssh -i %KEY% %USER% "find %REMOTE_FOLDER%/icons/ %REMOTE_FOLDER%/assets/ -type d -exec chmod 4750 {} \;"
