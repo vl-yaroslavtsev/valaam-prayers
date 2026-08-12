284cd86712ea5f5741cd607b34734b8f14e50e1d
Владимир Ярославцев <vl.yaroslavtsev@gmail.com>
2026-08-12 16:42:18 +0300

    Изменяем папку для деплоя

--- scripts/deploy.cmd
+++ scripts/deploy.cmd
@@ -5,6 +5,6 @@ set USER="valaam.ru@valaam.ru"
 set REMOTE_FOLDER="/var/www/app.valaam.ru/data"
 
 
-ssh %USER% "rm -rf %REMOTE_FOLDER%/app/*"
-scp -r ./www/* %USER%:%REMOTE_FOLDER%/app/
-ssh %USER% "find %REMOTE_FOLDER%/app/ -type d -exec chmod 4755 {} \;"
+ssh %USER% "rm -rf %REMOTE_FOLDER%/webview/*"
+scp -r ./www/* %USER%:%REMOTE_FOLDER%/webview/
+ssh %USER% "find %REMOTE_FOLDER%/webview/ -type d -exec chmod 4755 {} \;"
